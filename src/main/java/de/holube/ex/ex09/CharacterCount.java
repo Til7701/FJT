cb58640be16ee0c31f09c5856eb7db7093fe2147
Tilman Holube <tilman@holube.de>
2022-06-29 14:39:21 +0200

    ex09 a1u2

--- src/main/java/de/holube/ex/ex09/CharacterCount.java
+++ src/main/java/de/holube/ex/ex09/CharacterCount.java
@@ -1,13 +1,60 @@
 package de.holube.ex.ex09;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
 public class CharacterCount {
 
     public static void main(String[] args) {
         final String[] strings = {"hallo", null, "", "welt", "geradeaus",
-                "jahr", "achtung"};
+                "jahr", "achtung", "a", "ax", "xa"};
         final char ch = 'a';
         int result = numberOf(strings, ch);
         System.out.println(result);
+    }
+
+    public static int numberOf(String[] strings, char ch) {
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        List<Future<Integer>> futures = new ArrayList<>();
+
+        for (String s : strings) {
+            futures.add(executorService.submit(new SearchTask(s, ch)));
+        }
+        executorService.shutdown();
+
+        int result = 0;
+        boolean interrupted = Thread.interrupted();
+        for (Future<Integer> future : futures) {
+            boolean gotResult = false;
+            while (!gotResult) {
+                try {
+                    result += future.get();
+                    gotResult = true;
+                } catch (InterruptedException e) {
+                    interrupted = true;
+                } catch (ExecutionException e) {
+                    // ignore
+                }
+            }
+        }
+        if (interrupted) Thread.currentThread().interrupt();
+        return result;
+    }
+
+    private record SearchTask(String string, char ch) implements Callable<Integer> {
+
+        @Override
+        public Integer call() {
+            if (string == null || string.isEmpty()) return 0;
+
+            int counter = 0;
+            for (int i = 0; i < string.length(); i++) {
+                if (string.charAt(i) == ch) counter++;
+            }
+            return counter;
+        }
+
     }
 
 }
--- src/main/java/de/holube/ex/ex09/StringContains.java
+++ src/main/java/de/holube/ex/ex09/StringContains.java
@@ -1,15 +1,60 @@
 package de.holube.ex.ex09;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.*;
 
 public class StringContains {
 
     public static void main(String[] args) {
         final String[] strings = {"hallo", null, "", "welt", "geradeaus",
-                "jahr", "achtung"};
+                "jahr", "achtung", "a", "ax", "xa"};
         final char ch = 'a';
         List<String> result = contains(strings, ch);
         System.out.println(result);
     }
-    
+
+    public static List<String> contains(String[] strings, char ch) {
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        ExecutorCompletionService<String> completionService = new ExecutorCompletionService<>(executorService);
+
+        for (String s : strings) {
+            completionService.submit(new SearchTask(s, ch));
+        }
+        executorService.shutdown();
+
+        List<String> result = new ArrayList<>();
+        boolean interrupted = Thread.interrupted();
+        for (int i = 0; i < strings.length; i++) {
+            boolean gotResult = false;
+            while (!gotResult) {
+                try {
+                    Future<String> future = completionService.take();
+                    String string = future.get();
+                    if (string != null) result.add(string);
+                    gotResult = true;
+                } catch (InterruptedException e) {
+                    interrupted = true;
+                } catch (ExecutionException e) {
+                    // ignore
+                }
+            }
+        }
+        if (interrupted) Thread.currentThread().interrupt();
+        return result;
+    }
+
+    private record SearchTask(String string, char ch) implements Callable<String> {
+
+        @Override
+        public String call() {
+            if (string == null || string.isEmpty()) return null;
+
+            String c = "" + ch;
+            if (string.contains(c)) return string;
+            return null;
+        }
+
+    }
+
 }
