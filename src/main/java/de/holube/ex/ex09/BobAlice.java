fd66b992ff1d394bc0af04f1fa6f25cc013f8822
Tilman Holube <tilman.holube@t-online.de>
2022-07-01 12:48:14 +0200

    zettel 09 a3

--- src/main/java/de/holube/ex/ex09/BobAlice.java
+++ src/main/java/de/holube/ex/ex09/BobAlice.java
@@ -1,19 +1,33 @@
 package de.holube.ex.ex09;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadLocalRandom;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.concurrent.*;
 
 class Alice extends Thread {
 
-    private BlockingQueue<String> messages = new LinkedBlockingQueue<>();
+    private final BlockingQueue<String> messages = new LinkedBlockingQueue<>();
+
+    private final CryptoAG cryptoAG;
+
+    public Alice(CryptoAG cryptoAG) {
+        this.cryptoAG = cryptoAG;
+    }
 
     @Override
     public void run() {
         while (!interrupted()) {
             try {
                 String message = messages.take();
-                System.out.println(message);
+                System.out.println("Received: " + message);
+                System.out.println(cryptoAG.decrypt(message));
             } catch (InterruptedException e) {
                 interrupt();
             }
@@ -31,18 +45,23 @@ class Alice extends Thread {
 
 class Bob extends Thread {
 
-    private Alice alice;
+    private final Alice alice;
 
-    public Bob(String name, Alice alice) {
+    private final CryptoAG cryptoAG;
+
+    public Bob(String name, Alice alice, CryptoAG cryptoAG) {
         super(name);
         this.alice = alice;
+        this.cryptoAG = cryptoAG;
     }
 
     @Override
     public void run() {
         while (!interrupted()) {
             String message = "Greetings from " + getName() + "!";
-            alice.send(message);
+            String encrypted = cryptoAG.encrypt(message);
+            System.out.println("Sending: " + encrypted);
+            alice.send(encrypted);
             try {
                 Thread.sleep(ThreadLocalRandom.current().nextLong(1000));
             } catch (InterruptedException e) {
@@ -52,13 +71,65 @@ class Bob extends Thread {
     }
 }
 
+class CryptoAG {
+
+    private static final String ALGORITHM = "RSA";
+    private final KeyPair pair;
+
+    private final ExecutorService executorService;
+
+    public CryptoAG() throws NoSuchAlgorithmException {
+        executorService = Executors.newFixedThreadPool(5);
+
+        KeyPairGenerator generator = KeyPairGenerator.getInstance(ALGORITHM);
+        generator.initialize(2048);
+        pair = generator.generateKeyPair();
+    }
+
+    public String encrypt(String string) {
+        return getResultUninterruptibly(() -> encryptImpl(string));
+    }
+
+    public String decrypt(String string) {
+        return getResultUninterruptibly(() -> decryptImpl(string));
+    }
+
+    private String getResultUninterruptibly(Callable<String> callable) {
+        while (true) {
+            try {
+                return executorService.submit(callable).get();
+            } catch (InterruptedException e) {
+                // ignore
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private String encryptImpl(String string) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, pair.getPublic());
+        byte[] cipherText = cipher.doFinal(string.getBytes());
+        return Base64.getEncoder().encodeToString(cipherText);
+    }
+
+    private String decryptImpl(String string) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, pair.getPrivate());
+        byte[] plainText = cipher.doFinal(Base64.getDecoder().decode(string));
+        return new String(plainText);
+    }
+
+}
+
 public class BobAlice {
 
     public static void main(String[] args) throws Exception {
-        Alice alice = new Alice();
+        CryptoAG cryptoAG = new CryptoAG();
+        Alice alice = new Alice(cryptoAG);
         alice.start();
         for (int i = 1; i < 100; i++) {
-            Thread bob = new Bob("Bob-" + i, alice);
+            Thread bob = new Bob("Bob-" + i, alice, cryptoAG);
             bob.start();
             Thread.sleep(ThreadLocalRandom.current().nextLong(100));
         }
